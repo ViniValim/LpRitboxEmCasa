fd378f2cfc3ee59051292f3bc940c4aa52cb721f
Vinicius Valim <valim.vvd@gmail.com>
2022-12-14 04:49:19 -0300

    AjustesSlider

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -5,3 +5,19 @@ requireAll(require.context('../svg', true, /\.svg$/));
 requireAll(require.context('../images', true, /\.(png|jpe?g|gif)$/i));
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//
+
+let count = 1;
+document.getElementById("radio1").checked =true;
+
+setInterval (function (){
+    nextImage();
+}, 2000)
+
+function nextImage(){
+    count++;
+    if(count>5){
+        count = 1;
+    }
+document.getElementById("radio"+count).checked = true;
+
+}
